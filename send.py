c6b2c70ee4243e8d1c109893691690ef16053f32
Aline Brito <14023536+alinebrito@users.noreply.github.com>
2021-06-17 12:35:18 -0300

    improve: Add student name to log

--- send.py
+++ send.py
@@ -12,7 +12,7 @@ import util
 
 subject = "Certificado do Curso de Extensão em Engenharia de Software Moderna"
 body = """\
-Prezado aluno(a),
+Prezado(a) aluno(a),
 
 Segue em anexo:
 
@@ -25,11 +25,11 @@ Favor confirmar o recebimento desta mensagem.
 
 Cordialmente,
 
-Prof. Marco Tulio Valente
+Prof. André Hora
 Coordenador do curso.
 
 P.S. Para quem não fez, gostaríamos de solicitar o preenchimento do formulário de avaliação do curso, disponível em:
-xxxxxxxxx"""
+https://docs.google.com/forms/d/e/1FAIpQLSeRBzayt-D_gLBGsZBhpAIcWp0tfu4fUP2FZ2RRd5JAxO1nMA/viewform?usp=sf_link"""
 
 def add_file(path, file_name):
   with open(os.path.join(path, file_name), "rb") as attachment:
@@ -78,7 +78,8 @@ def run():
   for index, row in dataset.iterrows():
     student_email = row.get('email')
     student_code = row.get('numero')
-    print('\n------\nProcessing %i ...' % student_code)
+    student_name = row.get('nome')
+    print('\n------\nProcessing %i: %s ...' % (student_code, student_name))
     send_email(sender_email, password, student_email, student_code)
     print('Waiting 2 seconds...')
     sleep(2)
